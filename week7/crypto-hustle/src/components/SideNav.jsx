195119c921f610699e13b6799a3d7c96d122c41e
suzunak <suzunakimura@gmail.com>
2025-07-14 13:16:07 -0400

    initial commit

---
+++ week7/crypto-hustle/src/components/SideNav.jsx
@@ -0,0 +1,12 @@
+import CryptoNews from "./CryptoNews"
+
+const SideNav = () => {
+    return(
+        <div className="side-nav">
+            <h1>Here is my side bar</h1>
+            <CryptoNews />
+        </div>
+    )
+}
+
+export default SideNav;
