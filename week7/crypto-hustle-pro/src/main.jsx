c507b95d67ff09456a211df4efd7fd36c6c59de4
suzunak <suzunakimura@gmail.com>
2025-07-14 13:27:42 -0400

    Add Layout Route

--- week7/crypto-hustle-pro/src/main.jsx
+++ week7/crypto-hustle-pro/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Layout from './routes/Layout'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-        <Route index={true} path="/" element={<App />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App/>}/>
+        </Route>
     </Routes>
   </BrowserRouter>
 )
