8f072da58fded2ea4e149b3898fe974c58c1a0fc
suzunak <suzunakimura@gmail.com>
2025-07-14 13:22:39 -0400

    import routers

--- week7/crypto-hustle-pro/src/main.jsx
+++ week7/crypto-hustle-pro/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+        <Route index={true} path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>
 )
