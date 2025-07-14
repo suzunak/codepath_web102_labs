609871806f0e5819801657e39d185dea14736d8c
suzunak <suzunakimura@gmail.com>
2025-07-14 13:32:50 -0400

    Add route for DetailView

--- week7/crypto-hustle-pro/src/main.jsx
+++ week7/crypto-hustle-pro/src/main.jsx
@@ -4,12 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Layout from './routes/Layout'
+import DetailView from './routes/DetailView'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<App/>}/>
+          <Route path="/coinDetails/:symbol" element={<DetailView />}/>
         </Route>
     </Routes>
   </BrowserRouter>
