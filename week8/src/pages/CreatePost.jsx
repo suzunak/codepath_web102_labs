23a4e131521ba7ae194b158238391aae7b1f9396
suzunak <suzunakimura@gmail.com>
2025-07-21 13:46:49 -0400

    import folder

---
+++ week8/src/pages/CreatePost.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import './CreatePost.css'
+
+const CreatePost = () => {
+
+    const [post, setPost] = useState({title: "", author: "", description: ""})
+
+    const handleChange = (event) => {
+        const {name, value} = event.target
+        setPost( (prev) => {
+            return {
+                ...prev,
+                [name]:value,
+            }
+        })
+    }
+
+    return (
+        <div>
+            <form>
+                <label htmlFor="title">Title</label> <br />
+                <input type="text" id="title" name="title" onChange={handleChange} /><br />
+                <br/>
+
+                <label htmlFor="author">Author</label><br />
+                <input type="text" id="author" name="author" onChange={handleChange} /><br />
+                <br/>
+
+                <label htmlFor="description">Description</label><br />
+                <textarea rows="5" cols="50" id="description" name="description" onChange={handleChange}>
+                </textarea>
+                <br/>
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    )
+}
+
+export default CreatePost
---
+++ week8/src/pages/ReadPosts.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+import Card from '../components/Card'
+
+const ReadPosts = (props) => {
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        setPosts(props.data)
+    }, [props])
+    
+    return (
+        <div className="ReadPosts">
+            {
+                posts && posts.length > 0 ?
+                [...posts]
+                .sort((a, b) => a.id - b.id)
+                .map((post,index) => 
+                    <Card 
+                        key={post.id}
+                        id={post.id} 
+                        title={post.title}
+                        author={post.author}
+                        description={post.description}
+                    />
+                ) : <h2>{'No Challenges Yet 😞'}</h2>
+            }
+        </div>  
+    )
+}
+
+export default ReadPosts
