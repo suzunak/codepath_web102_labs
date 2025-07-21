c6504865d0fbde9e9af9838fdc2d88b91bd81617
suzunak <suzunakimura@gmail.com>
2025-07-21 13:52:40 -0400

    CreatePost

--- week8/src/pages/CreatePost.jsx
+++ week8/src/pages/CreatePost.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './CreatePost.css'
+import { supabase } from '../client'
+
 
 const CreatePost = () => {
 
@@ -13,6 +15,17 @@ const CreatePost = () => {
                 [name]:value,
             }
         })
+    }
+
+    const createPost = async (event) => {
+        event.preventDefault();
+
+        await supabase
+            .from('Posts')
+            .insert({title: post.title, author: post.author, description: post.description})
+            .select();
+
+        window.location = "/";
     }
 
     return (
@@ -30,7 +43,7 @@ const CreatePost = () => {
                 <textarea rows="5" cols="50" id="description" name="description" onChange={handleChange}>
                 </textarea>
                 <br/>
-                <input type="submit" value="Submit" />
+                <input type="submit" value="Submit" onClick={createPost}/>
             </form>
         </div>
     )
